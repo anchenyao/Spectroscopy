efec975c34f2ef8f95ff8ee1270ec5b090028f25
anchenyao <anchenyao@gmail.com>
2020-10-05 10:21:23 -0500

    Update spectroscopy_code.py

--- spectroscopy_code.py
+++ spectroscopy_code.py
@@ -6,6 +6,13 @@ import numpy as np
 valence = {'H':1, 'O':2, 'N':3, 'C':4}
 valence_inv = {1:'H', 2:'O', 3:'N', 4:'C'}
 
+#max atom size we are working with
+max_vertices = 30
+
+#vertex valence remaining (1) + C,O,N,H within 2 edges (4) + atom type (4) + carbon type count (80) + bond type count (17)
+per_vertex_dimension = 116
+
+
 bondTypes = []
 carbonBondTypes = []
 
@@ -41,7 +48,7 @@ def twoAway(adjM):
     M[i][i] -= degM[i]
   return M
 
-#returns a V x 4 matrix, where the columns represent [H, O, N, C] respectively within two edges
+#returns a V x 4 matrix, where the columns represent [H, O, N, C] respectively within exactly two edges
 def twoAwayPerVertex(edges, vertices):
   twoM = twoAway(edge_to_adj(edges))
   out = []
@@ -113,7 +120,7 @@ def countBonds(edges, vertices):
 
 def countCarbons(edges, vertices):
   cindex = carbonIndex()
-  out = [0] * len *
+  out = [0] * len(carbonTypes)
   for i in range(len(vertices)):
     if vertices[i] == 'C':
       bonded = []
@@ -123,6 +130,41 @@ def countCarbons(edges, vertices):
       bonded = tuple(sorted(bonded))
       out[cindex[bonded]] += 1
   return out
+
+'''maps data to max_v x (3 * max_v + 116) matrix
+col [0, 3*max_v - 1] is edge information
+col 3*max_v is val remaining
+col [3*max_v + 1,3*max_v + 4] is within 2 per-vertex info
+col [3*max_v + 5, 3*max_v + 8] is atom type
+col [3*max_v + 9, 3*max_v + 88] is carbon type count, same for all vertices
+col [3*max_v + 89, 3*max_v + 115] is bond type count, same for all vertices
+'''
+def featurize(edges, vertices):
+  carb_count = countCarbons(edges, vertices)
+  bond_count = countBonds(edges, vertices)
+  withinTwo = twoAwayPerVertex(edges, vertices)
+
+  out = [[0] * (3 * max_vertices + per_vertex_dimension) for i in range(len(vertices))]
+  for i in range(len(vertices)):
+    out[i][3 * max_vertices] = valence[vertices[i]]
+    out[i][3 * max_vertices + 4 + valence[vertices[i]]] = 1
+    for j in range(4): #within two edges
+      out[i][3 * max_vertices + 1 + j] = withinTwo[i][j]
+    for j in range(80): #carbon types
+      out[i][3 * max_vertices + 9 + j] = carb_count[j]
+    for j in range(17): #bond types
+      out[i][3 * max_vertices + 89 + j] = bond_count[j]
+
+
+  #map edge matrix to a max_v by 3*max_v binary indicator matrix
+  for i in range(len(edges)):
+    for j in range(i, len(edges), 1):
+      if edges[i][j] != 0:
+        out[i][3 * j + edges[i][j] - 1] = 1
+        out[i][3* max_vertices] -= edges[i][j]
+        out[j][3* max_vertices] -= edges[i][j]
+  return out
+
 
 def generateTrainingPair(edges):
   out = [[0 for i in range(len(edges))] for j in range(len(edges))]
@@ -164,4 +206,96 @@ def graph2TensorTo3Tensor(edges):
         out[i][j][edges[i][j] - 1] = 1
   return out
 
+
+
 ####CODE BODY####
+
+'''GRU implementation found online
+class GRUNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, n_layers, drop_prob=0.2):
+        super(GRUNet, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        
+        self.gru = nn.GRU(input_dim, hidden_dim, n_layers, batch_first=True, dropout=drop_prob)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x, h):
+        out, h = self.gru(x, h)
+        out = self.fc(self.relu(out[:,-1]))
+        return out, h
+    
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        hidden = weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device)
+        return hidden
+
+def train(train_loader, learn_rate, hidden_dim=256, EPOCHS=5, model_type="GRU"):
+    
+    # Setting common hyperparameters
+    input_dim = next(iter(train_loader))[0].shape[2]
+    output_dim = 1
+    n_layers = 2
+    # Instantiating the models
+    if model_type == "GRU":
+        model = GRUNet(input_dim, hidden_dim, output_dim, n_layers)
+    else:
+        model = LSTMNet(input_dim, hidden_dim, output_dim, n_layers)
+    model.to(device)
+    
+    # Defining loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
+    
+    model.train()
+    print("Starting Training of {} model".format(model_type))
+    epoch_times = []
+    # Start training loop
+    for epoch in range(1,EPOCHS+1):
+        start_time = time.clock()
+        h = model.init_hidden(batch_size)
+        avg_loss = 0.
+        counter = 0
+        for x, label in train_loader:
+            counter += 1
+            if model_type == "GRU":
+                h = h.data
+            else:
+                h = tuple([e.data for e in h])
+            model.zero_grad()
+            
+            out, h = model(x.to(device).float(), h)
+            loss = criterion(out, label.to(device).float())
+            loss.backward()
+            optimizer.step()
+            avg_loss += loss.item()
+            if counter%200 == 0:
+                print("Epoch {}......Step: {}/{}....... Average Loss for Epoch: {}".format(epoch, counter, len(train_loader), avg_loss/counter))
+        current_time = time.clock()
+        print("Epoch {}/{} Done, Total Loss: {}".format(epoch, EPOCHS, avg_loss/len(train_loader)))
+        print("Total Time Elapsed: {} seconds".format(str(current_time-start_time)))
+        epoch_times.append(current_time-start_time)
+    print("Total Training Time: {} seconds".format(str(sum(epoch_times))))
+    return model
+
+def evaluate(model, test_x, test_y, label_scalers):
+    model.eval()
+    outputs = []
+    targets = []
+    start_time = time.clock()
+    for i in test_x.keys():
+        inp = torch.from_numpy(np.array(test_x[i]))
+        labs = torch.from_numpy(np.array(test_y[i]))
+        h = model.init_hidden(inp.shape[0])
+        out, h = model(inp.to(device).float(), h)
+        outputs.append(label_scalers[i].inverse_transform(out.cpu().detach().numpy()).reshape(-1))
+        targets.append(label_scalers[i].inverse_transform(labs.numpy()).reshape(-1))
+    print("Evaluation Time: {}".format(str(time.clock()-start_time)))
+    sMAPE = 0
+    for i in range(len(outputs)):
+        sMAPE += np.mean(abs(outputs[i]-targets[i])/(targets[i]+outputs[i])/2)/len(outputs)
+    print("sMAPE: {}%".format(sMAPE*100))
+    return outputs, targets, sMAPE
+'''
+
